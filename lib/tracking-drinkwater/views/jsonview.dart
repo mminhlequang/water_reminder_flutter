9b9da117bfa773fc75d29a3b37bfa5674d565542
Minh Minh <mminh.lequang@gmail.com>
2022-11-01 10:53:51 +0700

    -

--- lib/tracking-drinkwater/views/jsonview.dart
+++ lib/tracking-drinkwater/views/jsonview.dart
@@ -12,45 +12,46 @@ import '../models/app_database_manager.dart';
 class WidgetJson extends StatelessWidget {
   const WidgetJson({super.key});
 
+  int totalOfMonth(int month) {
+    int total = 0;
+    AppPrefs.instance.logs
+        .where((e) =>
+            DateFormat('/MM/yyyy').format(e.date) ==
+            DateFormat('/MM/yyyy').format(DateTime(DateTime.now().year, month)))
+        .forEach((e) {
+      for (var e2 in e.records) {
+        total += e2.capacity;
+      }
+    });
+    return total;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Container(
-            child: JsonViewer({
-              'capacityChooserIndex': AppPrefs.instance.capacityChooserIndex,
-              'target': AppPrefs.instance.target,
-              'logs': AppPrefs.instance.logs
-                  .map((e) => {
-                        'datetime': DateFormat('dd/MM/yyyy').format(e.date),
-                        'records': e.records
-                            .map((e) => {
-                                  'capacity': e.capacity,
-                                  'time': e.time.format(context)
-                                })
-                            .toList()
-                      })
-                  .toList(),
-            }),
-            // child: FutureBuilder<List>(
-            //     future: AppDatabaseManager.instance.fetchAll(),
-            //     builder: (context, snapshot) {
-            //       if (snapshot.data == null) return const SizedBox();
-            //       return Column(
-            //         mainAxisSize: MainAxisSize.min,
-            //         children: snapshot.data!
-            //             .map((e) => Padding(
-            //                   padding: const EdgeInsets.all(12.0),
-            //                   child: Text(
-            //                     jsonEncode(e),
-            //                     textAlign: TextAlign.center,
-            //                   ),
-            //                 ))
-            //             .toList(),
-            //       );
-            //     }),
-          ),
+          child: JsonViewer({
+            'capacityChooserIndex': AppPrefs.instance.capacityChooserIndex,
+            'target': AppPrefs.instance.target,
+            'logsOfYear': List.generate(
+                12,
+                (index) => {
+                      'month': index + 1,
+                      "total": totalOfMonth(index + 1),
+                    }).toList(),
+            'logs': AppPrefs.instance.logs
+                .map((e) => {
+                      'datetime': DateFormat('dd/MM/yyyy').format(e.date),
+                      'records': e.records
+                          .map((e) => {
+                                'capacity': e.capacity,
+                                'time': e.time.format(context)
+                              })
+                          .toList()
+                    })
+                .toList(),
+          }),
         ),
       ),
     );
